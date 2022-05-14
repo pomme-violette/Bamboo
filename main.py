45675a77405c9f0a71950a65d3cc396bd966270d
pomme-violette <github@acc.ldtg.jp>
2022-05-14 23:53:10 +0900

    support extra pages.

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
 import os
 import re
-import requests
+import time
 import json
+import requests
 import lxml.html
-
 import chromedriver_binary
 from selenium import webdriver
-from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-def get_urls(url):
+def get_urls(url: str) -> list[str]:
     r = requests.get(url)
     html = lxml.html.fromstring(r.text)
     script_tags = html.xpath("//script")
-    formatting = script_tags[3].text.replace("'", '').replace(',', '')
-    formatted_list = re.findall("(?P<url>https?://[^\s]+)", formatting)
+    formatting = re.sub(r'[\'",]', '', script_tags[3].text)
+    formatted_list = re.findall("(?P<url>https?://[^\s]+.com[^\s]+)", formatting)
     return formatted_list
 
 
-def save_urls(file_name, url_list):
-    with open(file_name, 'w') as f:
-        for i in url_list:
-            f.write(i + '\n')
-    return file_name
-
-
-def download(download_url, file_dir, file_name):
+def download(download_url: str, file_dir: str, file_name: str):
     if not os.path.exists(file_dir):
-        os.mkdir(file_dir)
+        os.makedirs(file_dir)
     response = requests.get(download_url)
     with open(f"{file_dir}/{file_name}", "wb") as f:
         f.write(response.content)
-        # for CUI
         print(f"{download_url} -> {file_name}")
 
 
-def get_work_information(target_work_url, file_dir):
+def get_work_information(target_work_url: str) -> dict:
     options = Options()
     options.add_argument('--headless')
     options.add_experimental_option('detach', True)
     driver = webdriver.Chrome(options=options)
     driver.get(target_work_url)
+
+    time.sleep(5)
 
     episode_title = driver.find_element(
         by=By.XPATH, value="/html/body/div[4]/div[1]/div[1]")
@@ -50,7 +43,6 @@ def get_work_information(target_work_url, file_dir):
         by=By.XPATH, value="/html/body/div[4]/div[1]/div[2]")
     episode_next_publish_date = driver.find_element(
         by=By.XPATH, value="/html/body/div[4]/div[2]/div[1]/div[1]/p[2]")
-
     work_title = driver.find_element(
         by=By.XPATH, value="/html/body/div[4]/div[2]/div[1]/div[1]/h1")
     work_author = driver.find_element(
@@ -75,40 +67,51 @@ def get_work_information(target_work_url, file_dir):
 
     driver.close()
 
-    if not os.path.exists(file_dir):
-        os.mkdir(file_dir)
-
-    with open(f"{file_dir}/{work_title.text}_{episode_title.text}.json", 'w') as f:
-        json.dump(work_information, f, indent=4)
-
-    if os.path.exists(f"{file_dir}/{work_title.text}_{episode_title.text}.json"):
-        print(f"{work_title.text}_{episode_title.text}.json is saved")
-
     return work_information
 
 
+def save_works_information(file_dir: str, works_information: json) -> str:
+    with open(file_dir, 'w') as f:
+        json.dump(works_information, f, indent=4)
+
+    if os.path.exists(file_dir):
+        print(f"Metadata File -> {file_dir}")
+
+    return file_dir
+
+
+def save_img_urls(file_name: str, url_list: list[str]) -> str:
+    with open(file_name, 'w') as f:
+        for i in url_list:
+            f.write(i + '\n')
+
+    if os.path.exists(file_name):
+        print(f"Image File List -> {file_name}")
+    else:
+        return "File Not Found"
+
+    return file_name
+
+
 def main(target_work_url, base_output_path):
-    if not os.path.exists(base_output_path):
-        os.mkdir(base_output_path)
-
-    work_information = get_work_information(
-        target_work_url, f"{base_output_path}")
-
+    work_information = get_work_information(target_work_url)
     url_list = get_urls(target_work_url)
 
-    img_url_list_file_name = f"{work_information['title']}_{work_information['episode_title']}.txt"
+    if not os.path.exists(f"{base_output_path}/{work_information['title']}/{work_information['episode_title']}"):
+        os.makedirs(f"{base_output_path}/{work_information['title']}/{work_information['episode_title']}")
 
-    img_file_name = f"{work_information['title']}_{work_information['episode_title']}"
+    save_works_information(
+        f"{base_output_path}/{work_information['title']}/{work_information['episode_title']}/metadata.json",
+        work_information)
 
-    img_url_list_path = save_urls(
-        f"{base_output_path}/{img_url_list_file_name}", url_list)
-
-    if os.path.exists(img_url_list_path):
-        print(f"{img_url_list_path} is saved")
+    save_img_urls(f"{base_output_path}/{work_information['title']}/{work_information['episode_title']}/img_url.txt",
+                  url_list)
 
     for page_num, url in enumerate(url_list):
         download(
-            url, f"{base_output_path}/{img_file_name}", f"{page_num+1}.webp")
+            url,
+            f"{base_output_path}/{work_information['title']}/{work_information['episode_title']}/img",
+            f"{page_num + 1}.webp")
 
 
 if __name__ == '__main__':
